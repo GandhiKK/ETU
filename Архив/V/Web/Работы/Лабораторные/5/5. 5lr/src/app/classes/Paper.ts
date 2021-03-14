0f62073267ef3201db61ffa83111ae8646466cda
Konstantin <gandhi.noir@mail.ru>
2021-03-14 16:13:58 +0300

    fix web

--- Архив/V/Web/Работы/Лабораторные/5/5. 5lr/src/app/classes/Paper.ts
+++
@@ -1,3 +0,0 @@
-export class Paper {
-  constructor(public name: string, public rule: string, public max: number, public count: number, public startPrice: number) {}
-}
--- Архив/V/Web/Работы/Лабораторные/5/5. 5lr/src/app/classes/Partner.ts
+++
@@ -1,3 +0,0 @@
-export class Partner {
-  constructor(public name: string, public money: number) {}
-}
